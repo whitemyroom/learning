ca88b67d53ea5b7b8589d0e149af59c8338d6114
Gorbenko Oleg <gorbenko.it@gmail.com>
2014-10-15 16:53:18 +0600

    Update lesson4.php

--- lesson4.php
+++ lesson4.php
@@ -9,10 +9,10 @@
 <?php
 print_r($_REQUEST);
 
-function printSelect($myArray, $name, $options) {
-	$val = $myArray[$name];
+function makeSelect($array, $name, $options) {
+	$val = $array[$name];
 
-	$result = '';
+	$options_str = '';
 	$selected = '';
 
 	foreach ($options as $key => $text) {
@@ -22,25 +22,25 @@ function printSelect($myArray, $name, $options) {
 			$selected = '';
 		}
 
-		$result .= "<option " . $selected . " value=" . $key . ">" . $text . "</option>";
+		$options_str .= "<option " . $selected . " value=" . $key . ">" . $text . "</option>";
 	}
 
-	echo "<select name=" . $name . ">" . $result . "</select>";
+	echo "<select name=" . $name . ">" . $options_str . "</select>";
 }
 ?>
 </pre>
-	<form method="REQUEST">
+	<form method="post">
 		<label for="first-name">Имя:&nbsp;</label>
-		<input value="<?php echo $_REQUEST['first-name']; ?>" name="first-name" id="first-name">
+		<input value="<?php echo $_POST['first-name']; ?>" name="first-name" id="first-name">
 		<br>
 		<label>Домашние животные: </label>
-			<?php printSelect($_REQUEST, 'animals', array(
+			<?php printSelect($_POST, 'animals', array(
 				'Dog' => 'Собака',
 				'Cat' => 'Кошка',
 			)); ?> 
 		<br>
 		<label>Автомобиль: </label>
-			<?php printSelect($_REQUEST, 'cars', array(
+			<?php makeSelect($_POST, 'cars', array(
 				'BMW' => 'БМВ',
 				'Mersedes' => 'Мерседес',
 			)); ?>
